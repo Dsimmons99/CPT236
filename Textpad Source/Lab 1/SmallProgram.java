6d11d58290d1bba8cb400c20b1e3c75c8299583f
Dsimmons99 <77588401+Dsimmons99@users.noreply.github.com>
2021-01-18 11:21:08 -0500

    Adding folder to hold text version of code to use for dropbox in class.

---
+++ Textpad Source/Lab 1/SmallProgram.java
@@ -0,0 +1,13 @@
+/*
+ *  Author:       David Simmons
+ *  Date:         1/17/2021
+ *  Description:  This program is a small print line statement
+ *                simply printing "It Works!"
+ */
+
+
+public class SmallProgram {
+    public static void main(String[] args) {
+        System.out.println("It Works!");
+    }
+}
