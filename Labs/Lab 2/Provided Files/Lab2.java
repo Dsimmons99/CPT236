96c62c1cafc9481bbf22d60659c7de1e744dfbbb
Dsimmons99 <77588401+Dsimmons99@users.noreply.github.com>
2021-01-22 10:35:04 -0500

    Lab 2

---
+++ Labs/Lab 2/Provided Files/Lab2.java
@@ -0,0 +1,4 @@
+public class Lab Two
+   Public Static Void Main(String args)
+      system.Out.Print1n(This is Lab Two)
+      system.Out.Print1n(Thank you)
---
+++ Labs/Lab 2/src/Lab2.java
@@ -0,0 +1,14 @@
+/*
+ *  Author:       David Simmons
+ *  Date:         1/22/2021
+ *  Description:  This program was filled with errors and typos
+ *				  which have been fixed in order to be compiled and
+ *			      ran without issue.
+ */
+
+public class Lab2  {
+    public static void main(String[] args)  {
+        System.out.println("This is Lab Two");
+        System.out.println("Thank you");
+    }
+}
